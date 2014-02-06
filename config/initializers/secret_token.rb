26f48cf2b4eb3684bf63e7175a885133428527dd
Mladen Yakimov <yakimov89@gmail.com>
2014-02-06 17:51:23 -0500

    first commit newly generated rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brewmasters::Application.config.secret_key_base = '98e8032db967cd6493860de2f19d6b474417b0b4d6c262fed04d9df5937e122ed932993d390914ba8963b59399db2a1142a75485d66dd4bef56bc4ae29186c28'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Brewmasters::Application.config.session_store :cookie_store, key: '_brewmasters_session'
