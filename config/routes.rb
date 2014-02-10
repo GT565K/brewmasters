1c801256942c41d9a7a321c95a92fd89b0ead4f4
Mladen Yakimov <yakimov89@gmail.com>
2014-02-09 22:50:23 -0500

    fix path for assets and restructure template

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Brewmasters::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'application#index'
   
-  get '/about' => 'home#about'
+  get '/about' => 'application#about'
   
-  get '/index' => 'home#index'
+  get '/index' => 'application#index'
   
   
 
