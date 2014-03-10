b70f325fb87b58ad0da0d2cbf4eac22adc43217c
Mladen Yakimov <yakimov89@gmail.com>
2014-03-10 08:49:10 -0400

    Add recipe model
    [Fixes #67161178]

---
+++ db/migrate/20140309000605_create_recipes.rb
@@ -0,0 +1,18 @@
+class CreateRecipes < ActiveRecord::Migration
+  def change
+    create_table :recipes do |t|
+      t.integer :user_id
+      t.string :name
+      t.string :description
+      t.float :yield_size
+      t.float :boil_temperature
+      t.float :sparge_temperature
+      t.float :mash_temperature
+      t.integer :boil_duration
+      t.integer :sparge_duration
+      t.integer :mash_duration
+
+      t.timestamps
+    end
+  end
+end
