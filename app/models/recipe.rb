ccf7d1d5442f132f3224d5dc6cd28f3a0388c559
Mladen Yakimov <yakimov89@gmail.com>
2014-04-10 21:41:28 -0400

    update recipe annotation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,19 +2,17 @@
 #
 # Table name: recipes
 #
-#  id                 :integer          not null, primary key
-#  user_id            :integer
-#  name               :string(255)
-#  description        :string(255)
-#  yield_size         :float remove
-#  boil_temperature   :float remove
-#  sparge_temperature :float remove
-#  mash_temperature   :float
-#  boil_duration      :integer
-#  sparge_duration    :integer remove
-#  mash_duration      :integer
-#  created_at         :datetime
-#  updated_at         :datetime
+#  id                :integer          not null, primary key
+#  user_id           :integer
+#  name              :string(255)
+#  description       :string(255)
+#  water_grain_ratio :float
+#  beer_type         :string(255)
+#  mash_temperature  :float
+#  boil_duration     :integer
+#  mash_duration     :integer
+#  created_at        :datetime
+#  updated_at        :datetime
 #
 
 # add water_grain_ratio qt/lb
