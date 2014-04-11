50904b89a8eb8b42557affc73adab1aee743edc8
Mladen Yakimov <yakimov89@gmail.com>
2014-04-10 21:40:52 -0400

    create helper action to be used as a filter in order to verify the user auth token is valid

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -3,8 +3,9 @@ class Api::V1::ApiController < ApplicationController
   layout false
   skip_before_filter :verify_authenticity_token
   
-  def user_auth_token_is_valid!
+  def user_auth_token_is_valid
     user_token = params[:auth_token].presence
-    user       = user_token && User.find_by_authentication_token(user_token.to_s)
+    @user = User.find_by_auth_token(user_token.to_s)
+    return user_token && User.find_by_authentication_token(user_token.to_s)
   end
 end
