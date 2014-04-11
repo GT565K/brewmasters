1b2aa7e710ca4d104379d420ad08f4073220bdb9
Mladen Yakimov <yakimov89@gmail.com>
2014-04-11 00:06:49 -0400

    verify that recipe was created before returning HTTP request

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -15,14 +15,8 @@ class Api::V1::RecipesController < Api::V1::UsersController
   
   # get /recipes
   def index
-    puts "showing all recipes for user_id #{params[:user_id]}"
-    # puts @user
-    # puts params
-    
     user_recipes = Recipe.includes(:ingredients).where(user_id: @user.id)
-    #render nothing: true, status: 200
     if user_recipes
-      #.to_json(:include => :ingredients)
       render :json=> user_recipes.as_json(:except => [:id, :user_id]), :status=>200
     else
       render :json=> {:message=>"No recipes found"}, :status=>200
@@ -32,20 +26,14 @@ class Api::V1::RecipesController < Api::V1::UsersController
   
   # post /recipes
   def create
-    
-    puts "creating recipe with #{params[:recipe]}"
-    
-    # set user_id from found user
     params[:recipe][:user_id] = @user.id
-    
     recipe = Recipe.create(params[:recipe].permit!)
     
-    if recipe
+    if recipe.id
       render :json=> {:message=>"Recipe successfully created"}, status: 200
     else
       render :json=> recipe.errors, :status=>422
     end
-        
   end
   
   # put /recipes/:id
