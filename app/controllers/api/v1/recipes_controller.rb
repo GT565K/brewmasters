396e46455752733e7e3ad72c3eb46da6f6bbb903
Mladen Yakimov <yakimov89@gmail.com>
2014-04-10 23:29:40 -0400

    API for creating and retrieving recipes

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -16,9 +16,17 @@ class Api::V1::RecipesController < Api::V1::UsersController
   # get /recipes
   def index
     puts "showing all recipes for user_id #{params[:user_id]}"
-    puts @user
-    puts params
-    render nothing: true, status: 200
+    # puts @user
+    # puts params
+    
+    user_recipes = Recipe.includes(:ingredients).where(user_id: @user.id)
+    #render nothing: true, status: 200
+    if user_recipes
+      #.to_json(:include => :ingredients)
+      render :json=> user_recipes.as_json(:except => [:id, :user_id]), :status=>200
+    else
+      render :json=> {:message=>"No recipes found"}, :status=>200
+    end
     
   end
   
@@ -26,9 +34,17 @@ class Api::V1::RecipesController < Api::V1::UsersController
   def create
     
     puts "creating recipe with #{params[:recipe]}"
-    puts @user
-    puts params
-    render nothing: true, status: 200
+    
+    # set user_id from found user
+    params[:recipe][:user_id] = @user.id
+    
+    recipe = Recipe.create(params[:recipe].permit!)
+    
+    if recipe
+      render :json=> {:message=>"Recipe successfully created"}, status: 200
+    else
+      render :json=> recipe.errors, :status=>422
+    end
         
   end
   
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -22,4 +22,5 @@ class Recipe < ActiveRecord::Base
   
   belongs_to :user
   
+  accepts_nested_attributes_for :ingredients
 end
